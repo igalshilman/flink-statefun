a2bd5401676519e60a33cd264a537c99d8bfb8fe
Igal Shilman <igalshilman@gmail.com>
2020-02-26 17:06:12 +0100

    Add comments and rename the images

--- statefun-python-sdk/examples/flask/build-example.sh
+++ statefun-python-sdk/examples/flask/build-example.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# this part would be removed, once statefun will be released to PyPI
 cp ../../dist/statefun-1.1.0-py3-none-any.whl .
 
-docker build -f Dockerfile.python-worker . -t python-statefun-worker
+# build the flask container
+docker build -f Dockerfile.python-worker . -t flask-worker 
 
+# this part would be removed, once statefun will be released to PyPI
 rm -f statefun-1.1.0-py3-none-any.whl
 
-docker build -f Dockerfile.statefun . -t statefun-cluster
+# build the statefun Flink image
+docker build -f Dockerfile.statefun . -t flask-statefun 
 
