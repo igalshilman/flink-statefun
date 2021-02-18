1a9bd3c9b102a4c639fec17ba358f1cc42dcee0b
Igal Shilman <igalshilman@gmail.com>
2021-02-18 18:14:10 +0100

    Add initial JavaDoc

--- statefun-sdk-java/src/main/java/org/apache/flink/statefun/sdk/java/handler/RequestReplyHandler.java
+++ statefun-sdk-java/src/main/java/org/apache/flink/statefun/sdk/java/handler/RequestReplyHandler.java
@@ -5,5 +5,12 @@ import org.apache.flink.statefun.sdk.java.slice.Slice;
 
 public interface RequestReplyHandler {
 
+  /**
+   * Handles a {@code Stateful Functions} invocation.
+   *
+   * @param input a {@linkplain Slice} as received from the {@code StateFun} server.
+   * @return a serialized representation of the side-effects to preform by the {@code StateFun}
+   *     server, as the result of this invocation.
+   */
   CompletableFuture<Slice> handle(Slice input);
 }
